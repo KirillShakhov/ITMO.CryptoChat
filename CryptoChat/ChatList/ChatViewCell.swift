0030ff5155fbbba7124b02b68be4995e297e7e4f
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-05-14 20:23:09 +0300

    feat: chat list

---
+++ CryptoChat/ChatList/ChatViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChatViewCell.swift
+//  CryptoChat
+//
+//  Created by Кирилл Шахов on 14.05.2023.
+//
+
+import UIKit
+
+class ChatViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- CryptoChat/Home/MainViewController.swift
+++ CryptoChat/Home/MainViewController.swift
@@ -12,9 +12,35 @@ class MainViewController: UIViewController {
     
     @IBOutlet weak var ScanButton: UIButton!
     
+    private let refreshControl = UIRefreshControl()
+    @IBOutlet weak var chatList: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.chatList.register(UINib(nibName: "ChatViewCell", bundle: nil), forCellWithReuseIdentifier: "ChatViewCell")
+
+        self.chatList.dataSource = self
+        self.chatList.delegate = self
+
+        refreshControl.layer.zPosition = -1
+        self.chatList.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(updateChats(_:)), for: .valueChanged)
+        
+        refreshControl.tintColor = UIColor.systemPink.withAlphaComponent(0.4)
+        refreshControl.attributedTitle = NSAttributedString(string: "Update chats ...")
+
         // Do any additional setup after loading the view.
+    }
+    
+    @objc private func updateChats(_ sender: Any) {
+//        if(findField.text == nil || findField.text == ""){
+//            recipes = RecipeModel.getRecipes()
+//        }else{
+//            recipes = RecipeModel.getRecipes(find: findField.text!)
+//        }
+        self.refreshControl.endRefreshing()
+//        recipeList.reloadData()
+        print("Updated")
     }
 
     @IBAction func CreateQr(_ sender: Any) {
@@ -32,3 +58,65 @@ class MainViewController: UIViewController {
     }
 }
 
+extension MainViewController: UICollectionViewDelegate{
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ChatViewCell", for: indexPath) as! ChatViewCell
+//        let recipe = recipes[indexPath.item]
+//        cell.title.text = recipe.name
+//        cell.kcal.text = String(format: "%.1f", recipe.kcal)
+//        if(cell.imageURL != recipe.image){
+//            cell.image.image = nil
+//            cell.imageURL = recipe.image
+//            cell.activityIndicator.isHidden = false
+//            DispatchQueue.global().async {
+//                if let data = try? Data(contentsOf: URL(string: recipe.image)!) {
+//                    if let image = UIImage(data: data) {
+//                        DispatchQueue.main.async {
+//                            cell.image.image = image
+//                            cell.activityIndicator.isHidden = true
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//        cell.layer.shadowColor = UIColor.gray.cgColor
+//        cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+//        cell.layer.shadowRadius = 5.0
+//        cell.layer.shadowOpacity = 1.0
+//        cell.layer.masksToBounds = false
+//        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+        return cell
+    }
+}
+
+extension MainViewController: UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        let recipe = recipes[indexPath.item]
+        
+//        let storyboard = UIStoryboard(name: "RecipeCard", bundle: nil)
+//        guard let vc = storyboard.instantiateViewController(identifier: "RecipeCard") as? RecipeCard else { return }
+//
+//        vc.recipe = recipe
+//
+//        DispatchQueue.global().async {
+//            if let data = try? Data(contentsOf: URL(string: recipe.image)!) {
+//                if let image = UIImage(data: data) {
+//                    DispatchQueue.main.async {
+//                        vc.cardImage.image = image
+//                    }
+//                }
+//            }
+//        }
+//
+//
+////        let vc = storyboard.instantiateViewController(withIdentifier: "RecipeCard")
+//        vc.modalPresentationStyle = .fullScreen
+//
+////        self.navigationController?.pushViewController(vc, animated: true)
+//        present(vc, animated:true)
+        
+    }
+}
