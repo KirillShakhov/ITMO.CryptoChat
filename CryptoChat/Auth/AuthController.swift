5259067bcc85ef6aee48f5b324efa2938c6200a4
Кирилл Шахов <kirill.shakhov14@gmail.com>
2023-05-15 18:20:00 +0300

    feat: avatar choose

---
+++ CryptoChat/Auth/AuthController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ItEasy
+//
+//  Created by Кирилл Шахов on 03.01.2022.
+//
+
+import UIKit
+
+
+
+
+class AuthController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- CryptoChat/FirstSettings/FirstSettingsViewController.swift
+++ CryptoChat/FirstSettings/FirstSettingsViewController.swift
@@ -9,19 +9,55 @@ import UIKit
 
 class FirstSettingsViewController: UIViewController {
 
+    @IBOutlet weak var avatarView: UIView!
+    @IBOutlet weak var plusImage: UIImageView!
+    @IBOutlet weak var avatarImage: UIImageView!
+    @IBOutlet weak var uuidLabel: UILabel!
     @IBOutlet weak var nameField: UITextField!
+    var imagePicker = UIImagePickerController()
+
+    private var uuid: String = UUID().uuidString
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
         
         nameField.delegate = self
+        uuidLabel.text = "uuid: "+uuid
+        
+        let tapOnAvatar = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
+        avatarView.addGestureRecognizer(tapOnAvatar)
+        avatarImage.isHidden = true
     }
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
 
+    @IBAction func create(_ sender: Any) {
+        uuid = UUID().uuidString
+        uuidLabel.text = "uuid: "+uuid
+    }
+    
+    @objc func handleTap(_ sender: UITapGestureRecognizer? = nil) {
+        // handling code
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            print("Button capture")
+            
+            imagePicker.delegate = self
+            imagePicker.sourceType = .savedPhotosAlbum
+            imagePicker.allowsEditing = false
+            
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
 }
 extension FirstSettingsViewController:UITextFieldDelegate{
     func textFieldShouldReturn(_ textField: UITextField) -> Bool
@@ -36,4 +72,17 @@ extension FirstSettingsViewController:UITextFieldDelegate{
             }
             return true
         }
+}
+
+
+extension FirstSettingsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            self.avatarImage.image = pickedImage
+            self.avatarImage.isHidden = false
+            self.plusImage.isHidden = true
+        }
+
+        dismiss(animated: true, completion: nil)
+    }
 }
